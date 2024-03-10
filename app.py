aceb6f296b4f8eecdb0a73ecfe6dcff503953502
Eve Lin <evelin@buffalo.edu>
2024-03-09 23:07:53 -0500

    added image on screen with css and html

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
-from flask import Flask, render_template
-from pymongo import MongoClient
+from flask import Flask, render_template, send_from_directory
 
-mongo_client = MongoClient("mongo", 27018)
-db = mongo_client["cse312_Group_Project"]
-#chat_collection = db["chat"] can fill DB with collections as we need them
+app = Flask(__name__)
 
-app = Flask(__name__, template_folder='templates')
+@app.route('/static/<filename>')
+def serve_static(filename):
+    return send_from_directory('static', filename)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host= "0.0.0.0" ,debug=True, port=5050)
+    app.run(host="0.0.0.0", debug=True, port=5050)
---
+++ static/somethingJS.js
@@ -0,0 +1,5 @@
+// static/js/somethingJS.js
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('JavaScript loaded!');
+    // Add more JavaScript code as needed
+});
