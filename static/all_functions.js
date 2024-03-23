45fb7c48873119b9464fdee8e99c9849413b91a6
maggie <maggiechen259@gmail.com>
2024-03-23 18:20:11 -0400

    like functionality

--- static/all_functions.js
+++ static/all_functions.js
@@ -12,3 +12,52 @@ function welcome() {
 }
 
 
+document.addEventListener("DOMContentLoaded", function() {
+    //like buttons
+    const likeButtons = document.querySelectorAll(".like-button");
+    likeButtons.forEach(function(button) {
+        button.addEventListener("click", function() {
+            const postID = button.dataset.postID;
+            recordLike(postID);
+        });
+    });
+
+    //dislike buttons
+    const dislikeButtons = document.querySelectorAll(".dislike-button");
+    dislikeButtons.forEach(function(button) {
+        button.addEventListener("click", function() {
+            const postID = button.dataset.postID;
+            recordDislike(postID);
+        });
+    });
+});
+
+
+function recordLike(postID) {
+    fetch('/record-like', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json'
+        },
+        body: JSON.stringify({ postId: postID })
+    })
+}
+
+
+function recordDislike(postID) {
+    fetch('/record-dislike', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json'
+        },
+        body: JSON.stringify({ postId: postID })
+    })
+}
+
+
+// https://medium.com/swlh/javascript-how-to-create-a-like-button-3716c33b1879
+
+
+
