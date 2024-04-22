0968a2f62f5fc5a2494e90d0d1b16b278ccccab4
Anthony <anthony.m.arteaga@gmail.com>
2024-04-22 08:17:41 -0400

    small SocketIO change

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ import websockets
 # >>>>>>> 21249fd1dcb9d47421bbb0e99b2958b795e1a4db
 
 app = Flask(__name__)
-socketio = SocketIO(app, transports=['websocket'])
+socketio = SocketIO(app) #transports=['websocket']
 # app.config['SECRET_KEY'] = 'your_secret_key'
 # app.config['MONGO_URI'] = 'mongodb://localhost:27017/users'
 mongo_client = MongoClient("mongo", 27017)
